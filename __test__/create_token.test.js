3be72a9a4d526fbfda765fb3df76c48f81444599
AngelVillavicencio <villavicenciomunoz@gmail.com>
2023-08-24 11:37:07 -0500

    listo sin tests

--- __test__/create_token.test.js
+++
@@ -1,7 +0,0 @@
-
-const handler_create_token = require('../src/functions/create_token/handler');
-
-test('correct greeting is generated', () => {
-    expect(handler_create_token.main.getLocalGreeting("en")).toBe("Hello!");
-    expect(handler_create_token.main.getLocalGreeting("fr")).toBe("🌊");
-});
